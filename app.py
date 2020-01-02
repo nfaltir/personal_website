55184a3accaff5abde34a3e104c9f75c5b2b1d4b
Norvin Faltir <43119210+nfaltir@users.noreply.github.com>
2020-01-01 16:48:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
-
+#home page
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-
+#python projects, sql, R, data visualization
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-
+#page on resume doc
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
 
-
+#bio story
 @app.route("/bio")
 def bio():
     return render_template("bio.html")
